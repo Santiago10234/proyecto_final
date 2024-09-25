9fbbf5995ec50aad1e49ca11335a2f4947f77f5a
chenaoh <ariza.ps4@gmail.com>
2024-09-25 13:25:53 -0600

    path de cars

--- backend/paginaback/urls.py
+++ backend/paginaback/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 from usuarios.views import RegistroView
+from autos.views import CarListCreateAPIView, CarDetailAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('docs/', include_docs_urls(title = 'Api Documentation')),
-    path('api/registro/',RegistroView.as_view())
+    path('api/registro/',RegistroView.as_view()),
+    path('api/cars/', CarListCreateAPIView.as_view(), name='car_list_create'),
 ]
