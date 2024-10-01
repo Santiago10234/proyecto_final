6fdd0c31da9f379a84ccc450f798264ac201808d
chenaoh <ariza.ps4@gmail.com>
2024-10-01 09:45:54 -0600

    Mejoras en contact

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -1,35 +1,32 @@
 import React from 'react'
-
+import "../styles/contact.css"
 function FormContact() {
   return (
-    <div>
+    <div className='container-contact card'>
         <div className='head'>
             <h3>Contact Us Now</h3>
             <p>Say something to start a live chat</p>  
         </div>
         <form>
-            <fieldset disabled>
-                <div className="mb-3">
-                <label for="disabledTextInput" className="form-label">Disabled input</label>
-                <input type="text" id="disabledTextInput" className="form-control" placeholder="Disabled input"/>
-                </div>
-                <div className="mb-3">
-                <label for="disabledSelect" className="form-label">Disabled select menu</label>
-                <select id="disabledSelect" className="form-select">
-                    <option>Disabled select</option>
-                </select>
-                </div>
-                <div className="mb-3">
-                <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="disabledFieldsetCheck" disabled/>
-                    <label className="form-check-label" for="disabledFieldsetCheck">
-                    Can't check this
-                    </label>
-                </div>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </fieldset>
-        </form>
+        <fieldset>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">Name</label>
+            <input type="text" id="name" className="form-control" placeholder="Your Name" required />
+          </div>
+          
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email</label>
+            <input type="email" id="email" className="form-control" placeholder="Your Email" required />
+          </div>
+          
+          <div className="mb-3">
+            <label htmlFor="message" className="form-label">Message</label>
+            <textarea id="message" className="form-control" rows="5" placeholder="Type your message here" required></textarea>
+          </div>
+          
+          <button type="submit" className="btn btn-dark btn-contact w-100">Submit</button>
+        </fieldset>
+      </form>
         <div className='contact'>
             <i class="bi bi-whatsapp fs-3"></i>
             <i class="bi bi-envelope-fill fs-3"></i>   
